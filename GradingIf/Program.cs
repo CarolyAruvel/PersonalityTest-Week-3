5aaf75c715b2b1e049f1dde42b2d34eb15df8ed5
Opilane TTHK <opilane@tthk.ee>
2021-09-14 13:53:24 +0300

    PersonalityTest

---
+++ GradingIf/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GradingIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mis hinded sa said?");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            if (Number == 5)
+            {
+                Console.WriteLine("Suurepärane");
+            }
+            else if (Number == 4)
+            {
+                Console.WriteLine("Väga hea");
+            }
+            else if (Number == 3)
+            {
+                Console.WriteLine("Hea");
+            }
+            else if (Number == 2)
+            {
+                Console.WriteLine("Rahuldav");
+
+            }
+            else
+            {
+                Console.WriteLine("Puudulik");
+            }
+        }
+    }
+}
